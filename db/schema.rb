67afede1feb05335d60e96b6940d3ff143081d52
Gabriele <gabriele.buonaiuto@gmail.com>
2014-08-08 00:37:58 +0200

    Added Name to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807164315) do
+ActiveRecord::Schema.define(version: 20140807222719) do
 
   create_table "boxes", force: true do |t|
     t.string   "title"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20140807164315) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
